0c5b2d99dc02cf72267dce41cec1cf061879657f
Artem Kiselev <kiselevart1403@gmail.com>
2025-01-06 13:13:44 +0700

    debugging statements

--- lua/nvim-discord-rpc/init.lua
+++ lua/nvim-discord-rpc/init.lua
@@ -2,7 +2,10 @@ local M = {}
 
 function M.setup()
   local command = '../../bin/discord-rpc'
-  os.execute(command)
+  print(command)
+
+  local result = vim.fn.system(command)
+  print(result)
 end
 
 return M
