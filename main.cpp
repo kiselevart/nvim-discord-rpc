086184d85476dcd3cff5e5d3adad1fe867afc247
Artem Kiselev <kiselevart1403@gmail.com>
2025-01-06 09:36:56 +0700

    basic main.cpp created

--- main.cpp
+++ main.cpp
@@ -14,4 +14,29 @@ namespace {
 
 int main() {
   DiscordState state{};
+  discord::Core* core{};
+  auto response = discord::Core::Create(146946889156263936, DiscordCreateFlags_Default, &core);
+  state.core.reset(core);
+
+  if (!state.core) {
+    std::cerr << "Failed to instantiate Discord!";
+    std::exit(-1);
+  }
+
+  discord::Activity activity{};
+  activity.SetDetails("neovim");
+  activity.SetState("editing");
+  activity.SetType(discord::ActivityType::Playing);
+  state.core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {
+    std::cout << ((result == discord::Result::Ok) ? "Succeeded" : "Failed")
+      << " updating activity!\n";
+  });
+
+  std::signal(SIGINT, [](int) {
+    interrupted = true;
+  });
+  do {
+    state.core->RunCallbacks();
+    std::this_thread::sleep_for(std::chrono::milliseconds(16));
+  } while (!interrupted);
 }
